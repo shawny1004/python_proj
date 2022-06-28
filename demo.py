ec548d5f27130edf8e7b35f15d5e4ce8d2668250
Shuo Yang <shuo@Shuos-MacBook-Pro.local>
2022-06-28 01:52:17 -0700

    Lab1

---
+++ demo.py
@@ -0,0 +1,47 @@
+import shape
+
+## This lab demos inheritance
+
+## shape1 only has a name
+my_shape1 = shape.Shape("shape_1")
+print(f"shape_1's name is {my_shape1.get_name()}")
+## this will throw error 
+# print(my_shape1.get_area())
+
+## shape2 is a rectangle
+my_shape2 = shape.Rectangle("shape_2_rectangle", 20, 10)
+print(f"shape_2's name is {my_shape2.get_name()}")
+print(f"shape_2's area is {my_shape2.get_area()}")
+
+## shape3 is a square
+my_shape3 = shape.Square("shape_3_square", 20)
+print(f"shape_3's name is {my_shape3.get_name()}")
+print(f"shape_3's area is {my_shape3.get_area()}")
+
+## shape4 is a square with a hole
+my_shape4 = shape.SquareWithAHoleInside("shape_4_square_hole", 20, 1)
+print(f"shape_4's name is {my_shape4.get_name()}")
+print(f"shape_4's area is {my_shape4.get_area()}")
+
+
+## shape5 is a red circle
+my_shape5 = shape.Circle("shape_5_circle", 10)
+print(f"shape_5's name is {my_shape5.get_name()}")
+print(f"shape_5's area is {my_shape5.get_area()}")
+print(f"shape_5's color is {my_shape5.get_color()}")
+
+print("#############################")
+
+## use list
+## we can do all the thing use a list
+my_shapes = [my_shape1, my_shape2, my_shape3, my_shape4, my_shape5]
+for s in my_shapes:
+    try:
+        print(f"A {s.get_color()} {s.get_name()} has area of {s.get_area()}" )
+    except NotImplementedError:
+        print(f"{s.get_name()}'s area function is not implemented")
+
+## Type casting:
+## say if my square's hole is fixed now, I don't want to subtract the hole 
+my_shape4.__class__=shape.Square
+print(f"Now the area is {my_shape4.get_area()} without hole")
---
+++ shape.py
@@ -0,0 +1,64 @@
+
+from pickletools import read_unicodestring1
+
+PI=3.1416
+
+class Shape:
+
+    color = "blue"
+
+    def __init__(self, name):
+        self.name = name
+        self.area = 0
+    
+    def get_area(self):
+        raise NotImplementedError()
+
+    def get_name(self):
+        return self.name
+
+    def get_color(self):
+        return self.color      
+
+class Rectangle(Shape):
+    def __init__(self, name, length, width):
+        self.name=name
+        self.length=length
+        self.width=width
+
+    def get_area(self):
+        self.area=self.length*self.width
+        return self.area
+
+class Square(Rectangle):
+    def __init__(self, name, side):
+        self.name=name
+        self.side=side
+
+    def get_area(self):
+        self.area=self.side*self.side
+        return self.area    
+
+class SquareWithAHoleInside(Square):
+    def __init__(self, name, side, hole_size):
+        self.name=name
+        self.side=side
+        self.hole_size=hole_size
+
+    def get_area(self):
+        self.area=super().get_area()-self.hole_size
+        return self.area
+
+
+class Circle(Shape):
+    def __init__(self, name, radius):
+        self.name=name
+        self.radius=radius
+        self.color = "red"
+
+    def get_area(self):
+        self.area = PI*self.radius*self.radius
+        return self.area
+
+
+
